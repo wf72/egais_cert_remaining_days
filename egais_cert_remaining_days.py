bf5bcbf1d085be0d00cda41c918184a15031ef56
wf <mailforwf@gmail.com>
2016-05-24 12:04:18 +0500

    new

--- egais_cert_remaining_days.py
+++ egais_cert_remaining_days.py
@@ -8,7 +8,7 @@ import re
 import time
 
 
-def get_sert_exp_date(utm_url, sert_type):
+def get_cert_end_date(utm_url, sert_type):
     """ get date from url """
     try:
         page = urllib.request.urlopen(utm_url)
@@ -24,14 +24,14 @@ def get_sert_exp_date(utm_url, sert_type):
                 return m.group(1)
 
 
-def date_to_epoch(sert_date):
+def date_to_epoch(d):
     """Convert date to epoch"""
     pattern = '%Y-%m-%d %H:%M:%S'
-    return int(time.mktime(time.strptime(sert_date, pattern)))
+    return int(time.mktime(time.strptime(d, pattern)))
 
 
 if __name__ == '__main__':
     if sys.argv[1] and sys.argv[2]:
-        sert_date = get_sert_exp_date("http://%s:8080" % sys.argv[1], sys.argv[2])
+        sert_date = get_cert_end_date("http://%s:8080" % sys.argv[1], sys.argv[2])
         if sert_date:
             print(int((date_to_epoch(sert_date) - int(time.time()))/86400))
